e5edc1525457b62770c1af0b631f0f1d9887add8
CoderAT13 <969437072@qq.com>
2018-12-08 14:23:24 +0800

    Initial Commit

---
+++ QR_code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QR_code
+//
+//  Created by 张涵健 on 2018/12/8.
+//  Copyright © 2018 张涵健Coder.@. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
